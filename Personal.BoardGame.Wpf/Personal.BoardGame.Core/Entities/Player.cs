db2f1238d2420cf691df35fcf9a0d6a5ae78b2d7
roel.remmerie <roel.remmerie@student.howest.be>
2022-06-04 12:17:52 +0200

    Delete positionId

--- Personal.BoardGame.Wpf/Personal.BoardGame.Core/Entities/Player.cs
+++ Personal.BoardGame.Wpf/Personal.BoardGame.Core/Entities/Player.cs
@@ -9,9 +9,9 @@ namespace Personal.BoardGame.Core.Entities
     public class Player
     {
         Random random = new Random();
+
         public int horizontalValue;
         public int verticalValue;
-        //public string positionId;
         public int HorizontalValue
         {
             get { return horizontalValue; }
@@ -22,17 +22,11 @@ namespace Personal.BoardGame.Core.Entities
             get { return verticalValue; }
             set { verticalValue = value; }
         }
-        /*public string PositionId
-        {
-            get { return positionId; }
-            set { positionId = value; }
-        }
-        */
+        
         public Player()
         {
             HorizontalValue = random.Next(0,19);
             VerticalValue = random.Next(0,19);
-            //PositionId = $"{horizontalValue}-{verticalValue}";
         }
         public int SetHorizontal()
         {
